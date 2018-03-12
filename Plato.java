bd16485019430a17764b33edbc9e85b2c93a9beb
A. Cross <37139175+Psychross@users.noreply.github.com>
2018-03-12 13:46:09 +0100

    Update Plato.java

--- Plato.java
+++ Plato.java
@@ -1,42 +1,39 @@
 
 package Hosteleria.model;
 
-/*
-* Plato.java
-*
-* Creada el 07-mar-2018, 9:53:55
-*
-* Desarrollada por Rafael en la empresa Aula6J el día 07-mar-2018
-*
-* Puede contactar conmigo en mail callefalsa123@yahoo.com
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
-/*
+public class Plato extends Consumicion {
 
-/**
- *
- * @author Rafael
- * @version 1.01v
- * @date 07-mar-2018
- */
-public class Plato {
     private TipoPlato TipoPlato;
-    private String ingrediente;
-    private int precio;
-    
-    public void plato (String ingrediente, int precio, String tipo){
-        this.ingrediente = ingrediente;
-        this.precio = precio;
+    private String ingredientes;
+
+    public Plato(TipoPlato TipoPlato, String ingredientes, double precio, String nombre) {
+        super(precio, nombre);
+        this.TipoPlato = TipoPlato;
+        this.ingredientes = ingredientes;
     }
-    
-    public void getingrediente(){
-        this.ingrediente = ingrediente;
+
+    public String getIngredientes() {
+        return ingredientes;
     }
-    
-    public void getprecio(){
-        this.precio = precio;
-    }
-    
-    public void gettipo(){
+
+    public void getTipo() {
         this.TipoPlato = TipoPlato;
     }
+
+    public void ordenarAlimentos() {
+        String ingre = this.ingredientes;
+        String orden = "";
+        Pattern pattern = Pattern.compile("\\w+");
+        Matcher matcher = pattern.matcher(ingre);
+        while (matcher.find()) {
+            //System.out.println(matcher.group());
+            orden = orden + matcher.group() + "\n";
+            this.ingredientes = orden;
+        }
+
+    }
 }
+
